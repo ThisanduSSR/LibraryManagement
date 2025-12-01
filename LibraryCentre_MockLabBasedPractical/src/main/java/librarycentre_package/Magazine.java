da1a2149068716cc626f756a5c40a30e32b1b4d9
ThisanduSSR <thisanduranadheera67@gmail.com>
2025-12-01 10:27:38 +0530

    moxk lab based practical

--- LibraryCentre_MockLabBasedPractical/src/main/java/librarycentre_package/Magazine.java
+++ LibraryCentre_MockLabBasedPractical/src/main/java/librarycentre_package/Magazine.java
@@ -1,4 +1,32 @@
 package librarycentre_package;
 
-public class Magazine {
+public class Magazine extends Item {
+    private int issueNo;
+    private int publicationFrequency;
+    private String editor;
+
+    public Magazine(String title, String ISBN){
+        super(title, ISBN);
+    }
+    public int getIssueNo() {
+        return issueNo;
+    }
+    public void setIssueNo(int issueNo) {
+        this.issueNo = issueNo;
+    }
+    public int getPublicationFrequency(){
+        return publicationFrequency;
+    }
+    public void setPublicationFrequency(int publicationFrequency) {
+        this.publicationFrequency = publicationFrequency;
+    }
+    public String getEditor() {
+        return editor;
+    }
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+    public String toString() {
+        return "Magazine:"+getTitle() + "Editor:"+editor+"issue No:"+issueNo+"publication frequency:"+publicationFrequency;
+    }
 }
